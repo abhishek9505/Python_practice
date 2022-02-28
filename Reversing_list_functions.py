0fa00648ad26c1829e83c0879b4acba68154bad1
Abhishek <abhishek@MacBook-Air.local>
2022-02-28 18:25:06 +0530

    Adding reverse_list Swap_list py files

---
+++ Reversing_list_functions.py
@@ -0,0 +1,6 @@
+def Reverse(lst):
+    lst.reverse()
+    return lst
+
+lst=[3,5,7,9,11,13]
+print(Reverse(lst))
---
+++ Swap_lst_func.py
@@ -0,0 +1,10 @@
+# Swap function
+def swapList(newList):
+     
+    newList[0], newList[-1] = newList[-1], newList[0]
+ 
+    return newList
+     
+
+newList = [12, 35, 9, 56, 24]
+print(swapList(newList))
